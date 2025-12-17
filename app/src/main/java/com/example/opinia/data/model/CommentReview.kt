e8a7f94bf1be8ff7f549a56e3f99954a72268490
Kaan Akkök <kaanakkok@gmail.com>
2025-12-17 18:56:32 +0300

    CommentReview updated

--- app/src/main/java/com/example/opinia/data/model/CommentReview.kt
+++ app/src/main/java/com/example/opinia/data/model/CommentReview.kt
@@ -1,6 +1,9 @@
 package com.example.opinia.data.model
 
 import com.google.firebase.firestore.DocumentId
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 data class CommentReview(
     @DocumentId
@@ -11,4 +14,11 @@ data class CommentReview(
     val comment: String = "",
 
     val timestamp: Long = System.currentTimeMillis() // yeni yazılan yorumu üstte göstermek için
-)
+) {
+    val formattedDate: String
+        get() {
+            val date = Date(timestamp)
+            val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            return format.format(date)
+        }
+}
